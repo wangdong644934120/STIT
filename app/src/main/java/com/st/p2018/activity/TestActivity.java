8b203af89fc6b0209da906e5bbf6f5db60e75de2
wangdong <644934120@qq.com>
2018-11-16 14:55:39 +0800

    测试2

---
+++ app/src/main/java/com/st/p2018/activity/TestActivity.java
@@ -0,0 +1,16 @@
+package com.st.p2018.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.st.p2018.stit.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        int a=0;
+    }
+}
